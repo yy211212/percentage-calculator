7edad4755f54174c30ed739f9fecf5125ead6740
yy211212 <limyuyoung211212@gmail.com>
2024-11-06 16:15:00 +0800

    commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Enter Marks Obtained in 4 Subjects: ")
+
+math = int(input("maths :"))
+
+english = int(input("english :"))
+
+science = int(input("science :"))
+
+hindi = int(input("hindi :")
+
+sum = math+english+science+hindi
+
+print("sun of math,english,science and hindi")
+
+perc = (sum/400)*100
+
+print(end="Percentage Mark = ")
+
+print(perc)
